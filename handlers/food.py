2299d25d9c9734da045465f23d552bfcd7d4dcf4
ppk <ppkywe27@gmail.com>
2023-12-17 21:36:22 +0630

    bugs fix

--- handlers/food.py
+++ handlers/food.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Request, Depends, HTTPException
 from fastapi.logger import logger
 from models.schema import (
     CurrentUser,
-    FoodSchemaWithMeta,
+    FoodSchemaWithMeta,GetFoodSchema,
     FoodCategorySchemaWithMeta
 )
 from typing import List, Dict
@@ -29,15 +29,35 @@ async def get_food_categories(
     return {"foodCategory":categories,"meta":meta_data}
 
 
-@router.get("/foods", tags=["food"],response_model=FoodSchemaWithMeta)
+# @router.get("/foods", tags=["food"],response_model=FoodSchemaWithMeta)
+# async def get_food(
+# page: int = 1 , per_page: int=10,
+#     db: Session = Depends(get_db), current_user: CurrentUser = Depends(get_current_user)
+# ):
+#     count = db.query(FoodModel).count()
+#     meta_data =  pagination(page,per_page,count)
+#     post_data = db.query(FoodModel).order_by(desc(FoodModel.createdate)).limit(per_page).offset((page - 1) * per_page).all()
+#     return {"food":post_data,"meta":meta_data}
+
+
+@router.get("/foods/{id}", tags=["food"], response_model=Dict[str,GetFoodSchema])
+def get_food_byid(id: int, db: Session = Depends(get_db)):
+    food = db.get(FoodModel, id)
+    if not food:
+        raise HTTPException(status_code=404, detail="Food ID not found.")
+    return {"food":food}
+
+@router.get("/foods", tags=["food"], response_model=Dict[str,List[GetFoodSchema]])
 async def get_food(
-page: int = 1 , per_page: int=10,
+    shop:str="shop1",category:str="all",
     db: Session = Depends(get_db), current_user: CurrentUser = Depends(get_current_user)
 ):
-    count = db.query(FoodModel).count()
-    meta_data =  pagination(page,per_page,count)
-    post_data = db.query(FoodModel).order_by(desc(FoodModel.createdate)).limit(per_page).offset((page - 1) * per_page).all()
-    return {"food":post_data,"meta":meta_data}
+    if category=="all":
+        food = db.query(FoodModel).filter(FoodModel.shop==shop).order_by(desc(FoodModel.createdate)).all()
+        return {"food":food}
+    else:
+        food = db.query(FoodModel).filter(FoodModel.shop==shop,FoodModel.category_id==category).order_by(desc(FoodModel.createdate)).all()
+        return {"food":food}
 
 @router.get("/foods/filter/{category}", tags=["food"],response_model=FoodSchemaWithMeta)
 async def get_food_bycategory(
